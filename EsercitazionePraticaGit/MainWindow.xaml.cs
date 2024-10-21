7656e72b5f882fe52f3cd031cfd68571a8cdcd20
Maria Rossi <MARIA.ROSSI@local.ittcesena.it>
2024-10-21 09:22:34 +0200

    Release 0.0.1 : creazione classe Biblioteca

--- EsercitazionePraticaGit/MainWindow.xaml.cs
+++ EsercitazionePraticaGit/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,9 +15,9 @@ namespace EsercitazionePraticaGit
 {
     class Libro()
     {
-        private string titolo;
-        private string autore;
-        private int pagine;
+        public string titolo;
+        public string autore;
+        public int pagine;
 
         private Libro assegnazione(string titolo, string autore, int pagine)
         {
@@ -47,6 +48,54 @@ namespace EsercitazionePraticaGit
             {
                 return 3;
             }
+        }
+
+    }
+
+    class Bilioteca
+    {
+        private string nome,indirizzo,open_time,close_time;
+
+        private List<Libro> libri = new List<Libro>();
+
+        public void addLibro(Libro libro)
+        {
+            if (libri.Count < 1000)
+            {
+                libri.Add(libro);
+            }
+        }
+        
+        public bool CercaLibro(string titolo)
+        {
+            for (int i = 0; i < libri.Count; i++)
+            {
+                if (libri[i].titolo == titolo)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+            
+        }
+
+        public string CercaLibriAutore(string autore)
+        {
+            string lista = "";
+            for (int i = 0; i < libri.Count; i++)
+            {
+                if (libri[i].autore == autore)
+                {
+                    lista += libri[i].titolo;
+                }
+            }
+            return lista;
+        }
+
+        public int nLibri()
+        {
+            return libri.Count;
         }
 
     }
